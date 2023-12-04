383021056fc9ea503911f4e6e4437d7618fa11d0
Chad Whitacre <chad@zetaweb.com>
2023-12-03 22:03:18 -0500

    Starting on mirror script

--- mirror.js
+++ mirror.js
@@ -11,40 +11,52 @@ function reverse(s) {
   return s.split('').reverse().join('');
 }
 
-async function download(url, destdir) {
-  if (url.endsWith('/')) {
-    var fnqs = '';
-    var filename = 'index.html';
-  } else {
-    var fnqs = path.basename(url);
-    var filename = fnqs, querystring = '';
+function main(src, dest) {
+  var dest = path.resolve(dest);
+
+  async function recurse(filepath) {
+    const document = (await JSDOM.fromFile(filepath)).window.document;
+    const anchors = document.getElementsByTagName('a');
+
+    for (var i=0, a; a=anchors[i]; i++) {
+      a.removeAttribute('target');
+
+      const base = `file://${process.cwd()}`
+      if (a.href.startsWith(base)) {
+        throw heck;
+      }
+
+      if (a.href.startsWith(src)) {
+        a.href = '/' + a.href.slice(src.length-1);
+      }
+
+      if (a.href.endsWith('index.html')) {
+        a.href = a.href.slice(0, -'index.html'.length);
+      }
+    }
   }
 
-  if (fnqs.indexOf('?') > -1) {
-    [filename, querystring] = fnqs.split('?');
-    var emanelif = reverse(filename);
-    var idx = emanelif.indexOf('.');
-    var ext = reverse(emanelif.slice(0, idx));
-    var fn = reverse(emanelif.slice(idx+1));
-    fn = fn.replaceAll('_', '-').replaceAll('.', '-');
-    part = querystring.replaceAll('=', '-').replaceAll('&', '-');
-    filename = `${fn}-${part}.${ext}`
-  }
+  async function download(url) {
+    var filepath = dest + new URL(url).pathname;
 
-  filepath = path.join(destdir, filename);
-  if (!fs.existsSync(filepath)) {
+    if (filepath.endsWith('/')) {
+      var filepath = filepath + 'index.html';
+    }
+
+    fs.mkdirSync(path.dirname(filepath), { recursive: true });
+
     var file = fs.createWriteStream(filepath);
     console.log('downloading', url);
-    await https.get(url, function(response) {
+    https.get(url, function(response) {
       response.pipe(file);
       file.on('finish', function() {
         file.close();
+        recurse(filepath);
       });
     });
   }
 
-  return filename;
+  download(src);
 }
 
-
-download('https://singinghome.com/', '.')
+main('https://singinghome.com/', 'docs');
